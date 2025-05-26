ed9c5f080af67fdaaaa85133005d2f47411c3c7b
saperate <61527642+saperate@users.noreply.github.com>
2025-05-25 20:40:58 -0400

    Updated in response to elementals changes that broke this

--- src/main/java/dev/saperate/elements/example/AbilityExample1.java
+++ src/main/java/dev/saperate/elements/example/AbilityExample1.java
@@ -2,11 +2,12 @@ package dev.saperate.elements.example;
 
 import dev.saperate.elementals.data.Bender;
 import dev.saperate.elementals.elements.Ability;
+import dev.saperate.elementals.utils.SapsUtils;
 
 public class AbilityExample1 implements Ability {
     @Override
     public void onCall(Bender bender, long l) {
-        ExampleElement.getElementByName("example").getAbility(1).onCall(bender,l);
+        ExampleElement.getElement("example").getAbility(1).onCall(bender,l);
     }
 
     @Override
